083526f17161303354fff867386e7810d2915ec9
Z3TA <juan00conde@gmail.com>
2025-11-10 17:05:34 +0100

    fix: calibration knob done + some minor fixes on coordinates

--- src/app/components/InstrumentViewer/InstrumentsComponents/Altimeter/Altimeter.ts
+++ src/app/components/InstrumentViewer/InstrumentsComponents/Altimeter/Altimeter.ts
@@ -2,12 +2,18 @@ import { Component, Signal, signal, WritableSignal } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { AltimeterConfig, getAltimeterConfig } from './altimeter-config';
 
-interface Coordinates {
+
+interface Coordinates{
+    coordinateNumbers: CoordinatesNumbers[];
+    coordinateGap: CoordinatesGap[];
+}
+
+interface CoordinatesNumbers {
     x: number;
     y: number;
     number: number;
     degree?: number;
-    coordinatesGaps?: CoordinatesGap[];
+
 }
 
 interface CoordinatesGap {
@@ -16,6 +22,14 @@ interface CoordinatesGap {
     degree?: number;
     number?: number;
 }
+
+interface CoordinatesKnob {
+    x: number;
+    y: number;
+    degree?: number;
+    number?: number;
+}
+
 
 interface PneuMode {
     pneumatic: string,
@@ -32,18 +46,25 @@ export class Altimeter {
 
     ALTIMETER_WIDTH: number = 450;
     ALTIMETER_HEIGHT: number = 450;
-    ALTIMETER_CLOCK_OFFSET: number = 50;
+    ALTIMETER_CLOCK_OFFSET: number = 65;
     ALTIMETER_NUMBERS_OFFSET: number = 55;
     ALTIMETER_NUMBER_AMOUNT: number = 10;
     ALTIMETER_GAP_AMOUNT: number = 5;
-    ALTIMEGER_GAP_OFFSET: number = 20;
+    ALTIMETER_GAP_OFFSET: number = 20;
 
+    ALTIMETER_KNOB_POSITIONS: number = 30;
 
-    altimeterConfig = signal<AltimeterConfig>(getAltimeterConfig(this.ALTIMETER_WIDTH, this.ALTIMETER_HEIGHT, this.ALTIMETER_CLOCK_OFFSET, this.ALTIMETER_NUMBERS_OFFSET, this.ALTIMETER_NUMBER_AMOUNT, this.ALTIMETER_GAP_AMOUNT, this.ALTIMEGER_GAP_OFFSET)); // width, height, clock face offset, numbers offst, numberAmount
-    coordinates = signal<Coordinates[]>([]);
+    altimeterConfig = signal<AltimeterConfig>(getAltimeterConfig(this.ALTIMETER_WIDTH, this.ALTIMETER_HEIGHT, this.ALTIMETER_CLOCK_OFFSET, this.ALTIMETER_NUMBERS_OFFSET, this.ALTIMETER_NUMBER_AMOUNT, this.ALTIMETER_GAP_AMOUNT, this.ALTIMETER_GAP_OFFSET)); // width, height, clock face offset, numbers offst, numberAmount
+    coordinates = signal<Coordinates>({
+        coordinateNumbers: [],
+        coordinateGap: []
+    });
+
+    coordinatesKnob = signal<CoordinatesKnob[]>([])
     
     TOTAL_DEGREES: number = 360;
     FEET_PER_ALTIMETER: number = 1000;
+    BAROMETRIC_ALTITUDE_DEFAULT: number = 2882;
 
     DEFAULT_ALTITUDE: number = 10000;
     DEFAULT_DEGREES: number = 0;
@@ -60,14 +81,17 @@ export class Altimeter {
     }
     pneuModeSelected = signal<string>(this.pneuModes.pneumatic);
 
-    altitudeCalibration = signal<number>(2882);
+    altitudeCalibration = signal<number>(this.BAROMETRIC_ALTITUDE_DEFAULT);
+    currentKnobCalibrationDegrees = signal<number>(this.DEFAULT_DEGREES);
+
     literals = signal({
         calibrationAltitude: "IN. MG",
         altitudex1000ft: "1000"
     })
 
     constructor() {
-        const coords: Coordinates[]  = this.generateNumbersCoordinates();
+        const coords: Coordinates  = this.generateNumbersCoordinates();
+        // const coordsKnob: Coordinates[] = this.generateKnobCoordinates();
         this.coordinates.set(coords);
         console.log("Coordinates: ", this.coordinates());
     }
@@ -88,6 +112,28 @@ export class Altimeter {
         }else{
             this.pneuModeSelected.set(this.pneuModes.pneumatic)
         }
+    }
+
+    onBarometricPressure(operation: string): void{
+        let angle = (360 /this.ALTIMETER_KNOB_POSITIONS); 
+        if(operation == '+'){
+            this.currentKnobCalibrationDegrees.update((degree) => degree += angle)
+            this.altitudeCalibration.update(calibration => ++calibration);
+            return
+        }
+
+        if(operation == "-"){
+            this.currentKnobCalibrationDegrees.update((degree) => degree -= angle)
+            this.altitudeCalibration.update(calibration => --calibration);
+            return
+        }
+
+        if(operation === "reset"){
+            this.currentKnobCalibrationDegrees.set(this.DEFAULT_DEGREES)
+            this.altitudeCalibration.set(this.BAROMETRIC_ALTITUDE_DEFAULT);
+            return
+        }
+
     }
 
     startSimulation(): void {
@@ -112,25 +158,36 @@ export class Altimeter {
         return increment;
     }
 
-    generateNumbersCoordinates(): Coordinates[] {
-        const coordinates: Coordinates[] = [];
+    generateNumbersCoordinates(): Coordinates {
+        const coordinates: Coordinates = {
+            coordinateNumbers: [],
+            coordinateGap: []
+        };
         
         const radius = (this.altimeterConfig().clockWidth / 2) - this.altimeterConfig().numbersOffset;
         const gapRadius = (this.altimeterConfig().clockWidth / 2) - this.altimeterConfig().gapRadius;
         
+        //Numbers Coordinates
         for (let i = 0; i < this.altimeterConfig().numberAmount; i++) {
             const angle = (i * 2 * Math.PI) / this.altimeterConfig().numberAmount;
-            let coordinatesGap: CoordinatesGap[] = [];
-            for (let j = 0; j < this.ALTIMETER_GAP_AMOUNT*this.altimeterConfig().numberAmount; j++) {
-                const gapAngle = (j * 2 * Math.PI) / (this.ALTIMETER_GAP_AMOUNT * this.altimeterConfig().numberAmount);
-                coordinatesGap.push({ x: gapRadius * Math.cos(gapAngle - Math.PI/2), y: gapRadius * Math.sin(gapAngle - Math.PI/2), degree: gapAngle * 180/Math.PI  });
-            }
-            coordinates.push({ number: i, x: radius * Math.cos(angle - Math.PI/2), y: radius * Math.sin(angle - Math.PI/2), degree: angle * 180/Math.PI, coordinatesGaps: coordinatesGap });
+            coordinates.coordinateNumbers.push({ number: i, x: radius * Math.cos(angle - Math.PI/2), y: radius * Math.sin(angle - Math.PI/2), degree: angle * 180/Math.PI });
+        }
+
+        //Gap Coordinates
+        for (let j = 0; j < this.ALTIMETER_GAP_AMOUNT*this.altimeterConfig().numberAmount; j++) {
+            const gapAngle = (j * 2 * Math.PI) / (this.ALTIMETER_GAP_AMOUNT * this.altimeterConfig().numberAmount);
+            coordinates.coordinateGap.push({ x: gapRadius * Math.cos(gapAngle - Math.PI/2), y: gapRadius * Math.sin(gapAngle - Math.PI/2), degree: gapAngle * 180/Math.PI  });
         }
 
         return coordinates ;
 
     }
+
+    // generateKnobCoordinates(): CoordinatesKnob[]{
+    //     for(let i = 0; i<this.ALTIMETER_KNOB_POSITIONS; i++){
+    //         const angleKnob = (i )
+    //     }
+    // }
 
 
 }
